621e7264f9dd4b7f47da57f643c9d808dc930a01
xSurfuel2 <xSurfuel2@gmail.com>
2025-10-01 09:17:21 +0200

    v1.1

--- src/main/java/org/example/LectorTexto.java
+++ src/main/java/org/example/LectorTexto.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class App
+public class LectorTexto
 {
     public static void main(String[] args)
     {
