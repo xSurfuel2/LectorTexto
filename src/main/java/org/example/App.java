cbc0018f5cdc6d89402814b20aa5b5918d1d6b5e
xSurfuel2 <xSurfuel2@gmail.com>
2025-10-01 08:47:53 +0200

    v1

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+ package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        String ruta = "src/main/resources/entrada.txt"; // Ruta relativa al proyecto
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta)))
+        {
+            String linea;
+            while ((linea = br.readLine()) != null)
+            {
+                System.out.println(linea);
+            }
+        } catch (IOException e)
+        {
+            System.err.println("Error al leer el archivo.");
+        }
+    }
+}
